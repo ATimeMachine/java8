9ad0e9f86b422eaaa5c2addab3b29692361a982a
chenjian <chenjian@suneee.com>
2018-07-02 18:19:10 +0800

    自定义注解

--- src/main/java/com/chen/java8/example/annotation/FruitInfoUtil.java
+++ src/main/java/com/chen/java8/example/annotation/FruitInfoUtil.java
@@ -11,8 +11,8 @@ import java.lang.reflect.Field;
 public class FruitInfoUtil {
 
     public static void getFruitInfo(Class<?> clazz) {
-        String strFruitName = " 水果名称：";
-        String strFruitColor = " 水果颜色：";
+        String strFruitName = "水果名称：";
+        String strFruitColor = "水果颜色：";
         String strFruitProvicer = "供应商信息：";
 
         Field[] declaredFields = clazz.getDeclaredFields();
