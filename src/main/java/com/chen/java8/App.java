59292166d822921dc363999fda60873877eb3604
陈建 <jian.chen@sohan.hk>
2023-03-17 11:47:08 +0800

    第6次提交

--- src/main/java/com/chen/java8/App.java
+++ src/main/java/com/chen/java8/App.java
@@ -6,7 +6,7 @@ package com.chen.java8;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("第一次提交");
+        System.out.println("第8次提交");
         System.out.println("第7次提交 --> 改第6次提交");
         System.out.println("第3次提交");
         System.out.println("第4次提交");
